49d7adcff78252dbe1d9b484d4d21938ffdf872a
Arepazo <liammattewsdorian@gmail.com>
2026-03-14 23:58:51 -0400

    Fix puppeteer args by removing extra comma
    Removed unnecessary comma in puppeteer options

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        ,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
